4ec26116cf08c457a9f286f48ea72935f6d72ea6
Ian <iankosgey535@gmail.com>
2023-12-14 15:24:51 +0300

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(offwork) {
+  switch (offwork) {
+    case "bathe my dog":
+      return "This Saturday, I want to bathe my dog!";
+      break;
+    default:
+      return "This Saturday, I want to roller-skate!";
+  }
+}
+function mondayWork(study) {
+  switch (study) {
+    case 'work from home':
+      return "This Monday, I will work from home.";
+      break;
+    default:
+      return "This Monday, I will go to the office.";
+  }
+}
+
+function wrapAdjective(wrapper = '*') {
+    return function(adjective = 'a hard worker'){
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    }
+}
